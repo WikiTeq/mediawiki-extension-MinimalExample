df736f69b47b030c9363a72f0838c1a2fbcdbe0e
Daniel Scherzer <daniel@wikiteq.com>
2024-03-19 18:45:07 -0700

    SpecialDoesUserExist: set up request handling
    Demonstrate handling of subpages and request parameters. Actual functionality
    is not available yet.

--- src/SpecialDoesUserExist.php
+++ src/SpecialDoesUserExist.php
@@ -20,11 +20,40 @@ class SpecialDoesUserExist extends SpecialPage {
         // Add a summary of the page functionality
         $this->outputHeader();
 
-        // We don't care about the subpage at the moment
-        // Output a placeholder message until we implement this
-        $out = $this->getOutput();
-        $out->addHTML(
-            "<p>This is some example raw HTML output since <b>this page is not implemented yet</b>.</p>"
+        // The username can be given either as the `user` request parameter,
+        // or as a subpage. The subpage takes precedence
+        if ( $subpage !== null && $subpage !== '' ) {
+            $this->checkIfUserExists( $subpage );
+            return;
+        }
+        // Fetch a parameter from the web request; getText() will default to an
+        // empty string if the request does not include the parameter
+        $usernameParam = $this->getRequest()->getText( 'username' );
+        if ( $usernameParam !== '' ) {
+            $this->checkIfUserExists( $usernameParam );
+            return;
+        }
+
+        $this->showInputForm();
+    }
+
+    /**
+     * Display a form to choose a username to check.
+     */
+    private function showInputForm(): void {
+        $this->getOutput()->addWikiTextAsInterface(
+            "'''Unimplemented''': showing the input form."
+        );
+    }
+
+    /**
+     * Check if a user with the given name exists and show the answer.
+     *
+     * @param string $usernameToCheck
+     */
+    private function checkIfUserExists( string $usernameToCheck ): void {
+        $this->getOutput()->addWikiTextAsInterface(
+            "'''Unimplemented''': checking if the user `$usernameToCheck` exists."
         );
     }
 
