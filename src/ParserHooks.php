fb75a7aa2a8d61e3367d084e790443afd355a12f
Daniel Scherzer <daniel@wikiteq.com>
2024-03-20 16:08:24 -0700

    ParserHooks: mark PAGECONTENTMODEL as expensive
    If the limit on expensive operations has been reached, just return an empty
    string. Checking the content model for the current page by not specifying a
    page name does not count as an expensive operation because we assume that the
    content model must be wikitext.

--- src/ParserHooks.php
+++ src/ParserHooks.php
@@ -49,6 +49,12 @@ class ParserHooks implements ParserFirstCallInitHook {
      * since parser functions are used for wikitext we can just assume that the
      * content model is wikitext and avoid an expensive database lookup.
      * 
+     * - Since looking up a page content model is an "expensive" operation in
+     * the sense that it generally requires a database lookup, we will mark this
+     * parser function as "expensive"; the number of expensive parser function
+     * calls that can be used on a single page is limited by the configuration
+     * variable $wgExpensiveParserFunctionLimit, which defaults to 100.
+     * 
      * - For pages that do not exist, we cannot be sure what their content
      * model is going to be, so return an empty string.
      * 
@@ -59,8 +65,18 @@ class ParserHooks implements ParserFirstCallInitHook {
         Parser $parser,
         string $pagename
     ): string {
+        // Allow retrieving the current page content model without counting it
+        // as an expensive operation, because it isn't.
         if ( $pagename === '' ) {
             return CONTENT_MODEL_WIKITEXT;
+        }
+
+        // But creating a Title object is going to be expensive; only do so if
+        // the limit on expensive operations has not been reached yet.
+        if ( $parser->incrementExpensiveFunctionCount() ) {
+            // `true` if the limit has been exceeded, in which case we don't
+            // check and just return an empty string
+            return '';
         }
 
         $title = $this->titleFactory->newFromText( $pagename );
