41bbc94d0030c09eb252d5091dc60a3579739c12
Daniel Scherzer <daniel@wikiteq.com>
2024-03-19 19:03:27 -0700

    SpecialDoesUserExist: set up initial input form
    Requests a username with a text field, and has a custom submission label.
    Begins the use of HTMLForm, which will be expanded later with label messages
    and input validation.

--- src/SpecialDoesUserExist.php
+++ src/SpecialDoesUserExist.php
@@ -2,6 +2,7 @@
 
 namespace MediaWiki\Extension\MinimalExample;
 
+use HTMLForm;
 use SpecialPage;
 
 class SpecialDoesUserExist extends SpecialPage {
@@ -41,9 +42,40 @@ class SpecialDoesUserExist extends SpecialPage {
      * Display a form to choose a username to check.
      */
     private function showInputForm(): void {
-        $this->getOutput()->addWikiTextAsInterface(
-            "'''Unimplemented''': showing the input form."
+        // Fields that our form will have
+        $fields = [
+            'Name' => [
+                'label' => 'Username:',
+
+                // uses HTMLTextField for display
+                'type' => 'text',
+
+                // put the value in the `username` request parameter instead
+                // of the default `wpName` (since this input field is under
+                // the key 'Name').
+                'name' => 'username',
+            ],
+        ];
+        // Create the actual form; use OOUI for a nicer output
+        $form = HTMLForm::factory( 'ooui', $fields, $this->getContext() );
+
+        // Add a custom label for the submission button
+        $form->setSubmitText( 'Check if a user exists!' );
+
+        // Use GET submissions so that the requested username is in the URL;
+        // this isn't needed but makes things clearer
+        $form->setMethod( 'get' );
+
+        // We don't use a normal submission handler since we just want
+        // the submission to reload the page with the parameter, but this needs
+        // to be called with something
+        $form->setSubmitCallback(
+            fn () => false
         );
+
+        // Tell the form to show itself; we don't need to manually add it to
+        // the page output
+        $form->show();
     }
 
     /**
