d7b26e4deceb9b6438e31eb40867e603fc628769
Daniel Scherzer <daniel@wikiteq.com>
2024-03-21 17:33:31 -0700

    Markdown: add initial rendering
    Require and use the `league/commonmark` composer package for doing the actual
    rendering. Currently pinned to version 2.4.2 of that package.
    
    The markdown now has basic rendering, but further steps are needed to align it
    with what we want for a wiki page.

--- src/Markdown/MarkdownContentHandler.php
+++ src/Markdown/MarkdownContentHandler.php
@@ -2,7 +2,12 @@
 
 namespace MediaWiki\Extension\MinimalExample\Markdown;
 
+use Content;
+use League\CommonMark\CommonMarkConverter;
+use MediaWiki\Content\Renderer\ContentParseParams;
+use ParserOutput;
 use TextContentHandler;
+use TitleFactory;
 
 /**
  * Content handler to add support for markdown.
@@ -36,4 +41,34 @@ class MarkdownContentHandler extends TextContentHandler {
         return MarkdownContent::class;
     }
 
+    /**
+     * Convert the raw markdown into the corresponding parsed HTML.
+     *
+     * @param Content $content
+     * @param ContentParseParams $cpoParams
+     * @param ParserOutput &$parserOutput The output object to fill (reference).
+     */
+    protected function fillParserOutput(
+        Content $content,
+        ContentParseParams $cpoParams,
+        ParserOutput &$parserOutput
+    ) {
+        // Don't do the complicated parsing logic if we are not generating
+        // HTML
+        if ( !$cpoParams->getGenerateHtml() ) {
+            $parserOutput->setText( null );
+            return;
+        }
+
+        // Use the external `League\CommonMark` library to convert the markdown
+        // to HTML, rather than trying to implement that parsing ourselves. We
+        // prevent unsafe links, and for raw HTML we escape it the same way that
+        // the core parser does for raw HTML in wikitext.
+        $converter = new CommonMarkConverter( [
+            'allow_unsafe_links' => false,
+            'html_input' => 'escape',
+        ] );
+        $parsed = $converter->convert( $content->getText() );
+        $parserOutput->setText( $parsed );
+    }
 }
