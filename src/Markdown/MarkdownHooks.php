ee2c8896d335b8df55d0978855c5a5df6bcf5d80
Daniel Scherzer <daniel@wikiteq.com>
2024-03-21 17:23:54 -0700

    Markdown: register as default content model for `.md`
    Add a handler for the `ContentHandlerDefaultModelFor` hook to make pages that
    end with `.md` be markdown by default. The handler is in its own class in the
    markdown-specific directory rather than adding it to the existing class that is
    for the parser hook.

---
+++ src/Markdown/MarkdownHooks.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MediaWiki\Extension\MinimalExample\Markdown;
+
+use MediaWiki\Revision\Hook\ContentHandlerDefaultModelForHook;
+use Title;
+
+class MarkdownHooks implements ContentHandlerDefaultModelForHook {
+
+    /**
+     * This hook is called to determine the default content model for a new
+     * page - we want to register `.md` pages as markdown by default.
+     *
+     * @param Title $title The title being created
+     * @param string &$model Model name, can be changed since the parameter is
+     *   passed by reference
+     * @return bool|void True or no return value to continue or false to abort
+     */
+    public function onContentHandlerDefaultModelFor( $title, &$model ) {
+        if ( str_ends_with( $title->getText(), '.md' ) ) {
+            $model = MarkdownContent::CONTENT_MODEL;
+            // Prevent other hooks from changing the content model to something
+            // else
+            return false;
+        }
+    }
+
+}
