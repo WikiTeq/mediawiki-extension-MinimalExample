d4411f0e5640248f34671b55a3b212683e818b2f
Daniel Scherzer <daniel@wikiteq.com>
2024-03-21 23:00:36 -0700

    SyntaxHelp: add ResourceLoader module to add help link
    When a page is being edited (action is 'edit' or 'submit'), add the
    `ext.minimalexample.syntaxhelp` ResourceLoader module. That module will query
    the `getsyntaxhelp` API endpoint to determine if there is a page which provides
    help for the syntax of the current content model; if there is such a page, a
    link to it is added after the page title.
    
    The link is currently put directly after the page title, and is shown with the
    same styles as the title itself. In a follow-up commit I will add some CSS to
    add spacing between the title and the link, and style the link.

---
+++ src/SyntaxHelp/DisplayHooks.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MediaWiki\Extension\MinimalExample\SyntaxHelp;
+
+use MediaWiki\Hook\BeforePageDisplayHook;
+use OutputPage;
+use Skin;
+
+class DisplayHooks implements BeforePageDisplayHook {
+
+    /**
+     * This hook is called prior to outputting a page - we use it to add our
+     * ResourceLoader module with a help link for the syntax of the page content
+     * model, but only when the current page could exist as a wikipage and the
+     * page is being edited (the action is either 'edit' or 'submit', we do
+     * not at the moment recognize custom edit actions from other extensions).
+     *
+     * @param OutputPage $out
+     * @param Skin $skin
+     * @return void This hook cannot be aborted
+     */
+    public function onBeforePageDisplay( $out, $skin ): void {
+        if ( !$out->getTitle()->canExist() ) {
+            return;
+        }
+        $action = $out->getActionName();
+        if ( $action === 'edit' || $action === 'submit' ) {
+            $out->addModules( [ 'ext.minimalexample.syntaxhelp' ] );
+        }
+    }
+}
