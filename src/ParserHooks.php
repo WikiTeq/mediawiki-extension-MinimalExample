7ffe54c65c299076385c46ff8257952f260e2056
Daniel Scherzer <daniel@wikiteq.com>
2024-03-21 18:20:23 -0700

    Add an option to hide the note on Special:ChangeContentModel
    Serves as an example of registering user options. This may not be the most
    useful preference to add for a real extension, since content models are
    generally rarely changed and the note is not very big, and every new preference
    adds complexity. See the essay [[Just make it a user preference]] on
    MediaWiki.org for a good explanation of the problems new preferences can pose.
    But, it is a reasonable preference for this example extension and a good
    demonstration of how preferences are added and used.

--- src/ParserHooks.php
+++ src/ParserHooks.php
@@ -3,16 +3,20 @@
 namespace MediaWiki\Extension\MinimalExample;
 
 use MediaWiki\Hook\ParserFirstCallInitHook;
+use MediaWiki\Preferences\Hook\GetPreferencesHook;
 use MediaWiki\SpecialPage\Hook\SpecialPageBeforeFormDisplayHook;
+use MediaWiki\User\UserOptionsLookup;
 use Parser;
 use TitleFactory;
 
 class ParserHooks implements
+    GetPreferencesHook,
     ParserFirstCallInitHook,
     SpecialPageBeforeFormDisplayHook
 {
 
     private TitleFactory $titleFactory;
+    private UserOptionsLookup $userOptionsLookup;
 
     /**
      * Like special pages and API modules, hook handlers can have dependencies
@@ -20,9 +24,14 @@ class ParserHooks implements
      * huge change. Previously, services needed to be retrieved manually.
      * 
      * @param TitleFactory $titleFactory
+     * @param UserOptionsLookup $userOptionsLookup
      */
-    public function __construct( TitleFactory $titleFactory ) {
+    public function __construct(
+        TitleFactory $titleFactory,
+        UserOptionsLookup $userOptionsLookup
+    ) {
         $this->titleFactory = $titleFactory;
+        $this->userOptionsLookup = $userOptionsLookup;
     }
 
     /**
@@ -46,7 +55,8 @@ class ParserHooks implements
      * form; Special:ChangeContentModel is one such page, and the one we want
      * to add extra logic to. On that page we add a note at the top that uses
      * of the `PAGECONTENTMODEL` parser function may display outdated results
-     * after the content model of the target page changes.
+     * after the content model of the target page changes, but only show that
+     * note if the user hasn't disabled it.
      *
      * @param string $name
      * @param HTMLForm $form
@@ -56,10 +66,35 @@ class ParserHooks implements
         if ( $name !== 'ChangeContentModel' ) {
             return;
         }
+        // Only show the message if the user wants the reminder
+        $showNote = $this->userOptionsLookup->getBoolOption(
+            $form->getContext()->getUser(),
+            'minimalexample-changecontentmodel-pref'
+        );
+        if ( !$showNote ) {
+            return;
+        }
         $message = $form->getContext()->msg(
             'minimalexample-changecontentmodel-cache-note'
         );
         $form->addPreHtml( $message->parse() );
+    }
+
+    /**
+     * This hook is used to register a new user preference - we want to allow
+     * users to disable the note about changing the content model leading to
+     * outdated page renderings.
+     *
+     * @param User $user
+     * @param array &$defaultPreferences
+     */
+    public function onGetPreferences( $user, &$defaultPreferences ) {
+        // The default value is configured in extension.json
+        $defaultPreferences['minimalexample-changecontentmodel-pref'] = [
+            'type' => 'check',
+            'section' => 'editing/editor',
+            'label-message' => 'minimalexample-changecontentmodel-pref-label',
+        ];
     }
 
     /**
